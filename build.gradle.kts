7a51d0bdff0ac30cea90689fbb849269ca3d3e4c
DESKTOP-MJ25L1Q\chlrl <gisungcu@gmail.com>
2025-04-27 14:19:48 +0900

    add lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,8 @@ allprojects {
             implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
             implementation("org.springframework.boot:spring-boot-configuration-processor")
             implementation("org.jetbrains.kotlin:kotlin-reflect")
+            implementation("org.springframework.boot:spring-boot-starter-data-redis")
+            implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.2")
             testImplementation("org.springframework.boot:spring-boot-starter-test")
             testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
             testRuntimeOnly("org.junit.platform:junit-platform-launcher")
